91d7d33acdbbe3bdc51107451fca0120684cc373
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-03-18 09:50:22 +0530

    [vijay]RetriveDataFromJsonServer

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,18 +1,63 @@
+using Newtonsoft.Json;
 using NUnit.Framework;
+using RestSharp;
+using RESTSharpTestDay30;
+using System;
+using System.Collections.Generic;
+using System.Net;
 
 namespace TestProject1
 {
+    public class Employee
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
     public class Tests
     {
+        RestClient client;
         [SetUp]
         public void Setup()
         {
+            client = new RestClient(" http://localhost:3000/Employee");
         }
-
+        /// <summary>
+        /// Called when [calling list return employee list]. UC1
+        /// </summary>
         [Test]
-        public void Test1()
+        public void OnCallingList_ReturnEmployeeList()
         {
-            Assert.Pass();
+            IRestResponse responce = GetEmployeeList();
+            Assert.AreEqual(responce.StatusCode, HttpStatusCode.OK);
+            List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(responce.Content);
+            Assert.AreEqual(8, dataResponse.Count);
+
+            foreach(Employee e in dataResponse)
+            {
+                Console.WriteLine("id" + e.id + "Name" + e.Name + "Salary" + e.Salary);
+            }
+        }
+        private IRestResponse GetEmployeeList()
+        {
+            
+        }
+        [Test]
+        public void GivenEmployee_OnPost_ShouldReturnAddedEmployee()
+        {
+            RestRequest request = new RestRequest("/Employee", Method.POST);
+            JsonObject JObjectBody = new JsonObject();
+            JObjectBody.Add("Name", "Chopper");
+            JObjectBody.Add("Salary", "5000");
+            request.AddParameter("application/json", JObjectBody, ParameterType.RequestBody);
+            IRestResponse responce = client.Execute(request);
+
+            //assert
+            Assert.AreEqual(responce.StatusCode, HttpStatusCode.Created);
+            Employee DataResponce = JsonConvert.DeserializeObject<Employee>(responce.Content);
+            Assert.AreEqual("Chopper", DataResponce.Name);
+            Assert.AreEqual("5000", DataResponce.Salary);
+            Console.WriteLine(responce.Content);
         }
     }
 }
